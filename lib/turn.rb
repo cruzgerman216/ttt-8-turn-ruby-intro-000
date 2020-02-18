6787c05fb6a082ccdf28c04b58043f6f7cf9c698
German Cruz <cruzgerman216@yahoo.com>
2020-02-18 20:01:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,10 +10,6 @@ def turn(board)
   puts "Please enter 1-9:"
 end
 
-def input_to_index(index)
-
-end
-
 def valid_move(index)
   if index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 || index == 8
     true
