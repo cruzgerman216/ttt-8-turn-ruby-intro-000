81f9b590149e37ac6e3155e930669850bcf036f7
German Cruz <cruzgerman216@yahoo.com>
2020-02-18 19:58:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,28 @@ end
 def turn(board)
   puts "Please enter 1-9:"
 end
+
+def input_to_index(index)
+
+end
+
+def valid_move(index)
+  if index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 || index == 8
+    true
+  else
+    false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, index)
+  if valid_move(index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      true
+    else
+      false
+    end
+  else
+    false
+  end
+end
